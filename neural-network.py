d4b513a99c916e74041604b8fcc0bac620bf53dc
Adam Steinberger <a.steinberger@leasetrack.ai>
2021-07-14 14:38:48 -0400

    feat: add runmanager

--- neural-network.py
+++ neural-network.py
@@ -1,4 +1,5 @@
-from pickletools import optimize
+from collections import namedtuple
+from lib2to3.pgen2.pgen import DFAState
 import torch
 import torch.nn as nn
 import torch.nn.functional as F
@@ -7,32 +8,19 @@ import torch.optim as optim
 import torchvision
 import torchvision.transforms as transforms
 
-import matplotlib.pyplot as plt
-from sklearn.metrics import confusion_matrix
-from resources.plotcm import plot_confusion_matrix
-
+from collections import OrderedDict
 from itertools import product
+
+import time
+import datetime
+import pandas as pd
+import json
 
 from torch.utils.tensorboard import SummaryWriter
 
 torch.set_printoptions(linewidth=120)
 torch.set_grad_enabled(True)
-
-def get_num_correct(preds, labels):
-    return preds.argmax(dim=1).eq(labels).sum().item()
-
-@torch.no_grad()
-def get_all_preds(model, loader):
-    all_preds = torch.tensor([]).cuda()
-    for batch in loader:
-        images, labels = batch
-        images, labels = images.cuda(), labels.cuda()
-        preds = model(images)
-        all_preds = torch.cat(
-            (all_preds, preds)
-            ,dim=0
-        ).cuda()
-    return all_preds
+pd.set_option('display.width', 120)
 
 class Network(nn.Module):
     def __init__(self):
@@ -57,6 +45,110 @@ class Network(nn.Module):
 
         return t
 
+class RunBuilder():
+    @staticmethod
+    def get_runs(params):
+        Run = namedtuple('Run', params.keys())
+        runs = []
+        for v in product(*params.values()):
+            runs.append(Run(*v))
+        return runs
+
+class Epoch():
+    def __init__(self):
+        self.count = 0
+        self.loss = 0
+        self.num_correct = 0
+        self.start_time = None
+
+class Run():
+    def __init__(self):
+        self.params = None
+        self.count = 0
+        self.data = []
+        self.start_time = None
+
+class RunManager():
+    def __init__(self):
+        self.epoch = Epoch()
+        self.run = Run()
+        self.network = None
+        self.loader = None
+        self.tb = None
+
+    def begin_run(self, run, network, loader):
+        self.run.start_time = time.time()
+        self.run.params = run
+
+        self.network = network
+        self.loader = loader
+
+        self.tb = SummaryWriter(comment=f'-{run}')
+
+        images, labels = next(iter(self.loader))
+        images, labels = images.cuda(), labels.cuda()
+
+        grid = torchvision.utils.make_grid(images)
+
+        self.tb.add_image('images', grid)
+        self.tb.add_graph(self.network, images)
+
+    def end_run(self):
+        self.tb.close()
+        self.epoch.count = 0
+
+    def begin_epoch(self):
+        self.epoch.start_time = time.time()
+        self.epoch.count += 1
+        self.epoch.loss = 0
+        self.epoch.num_correct = 0
+
+    def end_epoch(self):
+        epoch_duraction = time.time() - self.epoch.start_time
+        run_duration = time.time() - self.run.start_time
+
+        loss = self.epoch.loss / len(self.loader.dataset)
+        accuracy = self.epoch.num_correct / len(self.loader.dataset)
+
+        self.tb.add_scalar('Loss', loss, self.epoch.count)
+        self.tb.add_scalar('Accuracy', accuracy, self.epoch.count)
+
+        for name, param in self.network.named_parameters():
+            self.tb.add_histogram(name, param, self.epoch.count)
+            self.tb.add_histogram(f'{name}.grad', param.grad, self.epoch.count)
+
+        results = OrderedDict()
+        results["run"] = self.run.count
+        results["epoch"] = self.epoch.count
+        results["loss"] = loss
+        results["accuracy"] = accuracy
+        results["epoch duration"] = epoch_duraction
+        results["run duration"] = run_duration
+        for k, v in self.run.params._asdict().items(): results[k] = v
+        
+        self.run.data.append(results)
+        df = pd.DataFrame.from_dict(self.run.data, orient='columns')
+        print(df)
+
+    def track_loss(self, loss):
+        self.epoch.loss += loss.item() * self.loader.batch_size
+    
+    def track_num_correct(self, preds, loaders):
+        self.epoch.num_correct += self._get_num_correct(preds, loaders)
+
+    @torch.no_grad()
+    def _get_num_correct(self, preds, labels):
+        return preds.argmax(dim=1).eq(labels).sum().item()
+
+    def save(self, fileName):
+        pd.DataFrame.from_dict(
+            self.run.data
+            ,orient='columns'
+        ).to_csv(f'{fileName}.csv')
+
+        with open(f'{fileName}.json', 'w', encoding='utf-8') as f:
+            json.dump(self.run.data, f, ensure_ascii=False, indent=4)
+
 train_set = torchvision.datasets.FashionMNIST(
     root='./data'
     ,train=True
@@ -66,82 +158,37 @@ train_set = torchvision.datasets.FashionMNIST(
     ])
 )
 
-parameters = dict(
+params = OrderedDict(
     lr = [.01, .001]
     ,batch_size = [10, 100, 1000]
+    ,num_epochs = [5, 10]
 )
-param_values = [v for v in parameters.values()]
 
-for lr, batch_size in product(*param_values):
-    train_loader = torch.utils.data.DataLoader(train_set, batch_size=batch_size)
-
-    comment = f' batch_size={batch_size} lr={lr}'
-    tb = SummaryWriter(comment=comment)
+m = RunManager()
+for run in RunBuilder.get_runs(params):
 
     network = Network()
     network = network.cuda()
+    loader = torch.utils.data.DataLoader(train_set, batch_size=run.batch_size)
+    optimizer = optim.Adam(network.parameters(), lr=run.lr)
+    
+    m.begin_run(run, network, loader)
+    for epoch in range(run.num_epochs):
+        m.begin_epoch()
+        for batch in loader:
 
-    images, labels = next(iter(train_loader))
-    images, labels = images.cuda(), labels.cuda()
-    grid = torchvision.utils.make_grid(images)
-
-    tb.add_image('images', grid)
-    tb.add_graph(network, images)
-    tb.close()
-
-    optimizer = optim.Adam(network.parameters(), lr=lr)
-
-    for epoch in range(10):
-        total_loss = 0
-        total_correct = 0
-
-        for batch in train_loader:
             images, labels = batch
             images, labels = images.cuda(), labels.cuda()
-
             preds = network(images) # feed batch forward through the network
             loss = F.cross_entropy(preds, labels) # calculate the loss
-
             optimizer.zero_grad() # zero out the gradients
             loss.backward() # calculate the gradients
             optimizer.step() # update the weights
 
-            total_loss += loss.item() * batch_size
-            total_correct += get_num_correct(preds, labels)
+            m.track_loss(loss)
+            m.track_num_correct(preds, labels)
 
-        tb.add_scalar('Loss', total_loss, epoch)
-        tb.add_scalar('Number Correct', total_correct, epoch)
-        tb.add_scalar('Accuracy', total_correct / len(train_set), epoch)
-
-        for name, weight in network.named_parameters():
-            tb.add_histogram(name, weight, epoch)
-            tb.add_histogram(f'{name}.grad', weight.grad, epoch)
-
-        print("batch_size:", batch_size, "lr:", lr, "epoch:", epoch, "total_correct:", total_correct, "loss:", total_loss)
-
-    train_preds = get_all_preds(network, train_loader)
-    preds_correct = get_num_correct(train_preds.cuda(), train_set.targets.cuda())
-
-    print("total correct:", preds_correct)
-    print("accuracy:", preds_correct / len(train_set))
-
-    stacked = torch.stack(
-        (
-            train_set.targets.cuda()
-            ,train_preds.argmax(dim=1).cuda()
-        )
-        ,dim=1
-    )
-
-    cmt = torch.zeros(10, 10, dtype=torch.int32)
-
-    for p in stacked.cuda():
-        tl, pl = p.tolist()
-        cmt[tl, pl] = cmt[tl, pl] + 1
-
-    print(cmt.cuda())
-
-# cm = confusion_matrix(train_set.targets.cpu(), train_preds.argmax(dim=1).cpu())
-# names = ('T-shirt/top', 'Trouser', 'Pullover', 'Dress', 'Coat', 'Sandal', 'Shirt', 'Sneaker', 'Bag', 'Ankle boot')
-# plt.figure(figsize=(10,10))
-# plot_confusion_matrix(cm, names)
+        m.end_epoch()
+    m.end_run()
+now = datetime.datetime.now().__str__().translate ({ord(c): "-" for c in "!@#$%^&*()[]{};:,./<>?\|`~-=_+ "})
+m.save(f'results-{now}')
